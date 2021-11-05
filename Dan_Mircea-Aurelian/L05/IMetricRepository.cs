13487442d23bb226c162072d4957bffa7f94af40
Mircea Dan <danaurelianmircea@yahoo.com>
2021-11-06 00:26:12 +0200

    Laborator5-DATC-DanMirceaAurelian

---
+++ Dan_Mircea-Aurelian/L05/IMetricRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05
+{
+    interface IMetricRepository
+    {
+        public void GenerateMetric();
+    }
+}
---
+++ Dan_Mircea-Aurelian/L05/Repository/StudentsRepository.cs
@@ -0,0 +1,48 @@
+﻿using L05.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L05.Repository
+{
+    public class StudentsRepository:IStudentsRepository
+    {
+        private CloudTableClient _tableClient;
+        private CloudTable _studentsTable;
+        private string _connectionString;
+
+        private async Task InitializeTable()
+        {
+            var account = CloudStorageAccount.Parse(_connectionString);
+            _tableClient = account.CreateCloudTableClient();
+            _studentsTable = _tableClient.GetTableReference("studenti");
+            await _studentsTable.CreateIfNotExistsAsync();
+        }
+
+        public StudentsRepository()
+        {
+            _connectionString = "DefaultEndpointsProtocol=https;AccountName=laborator4danmircea;AccountKey=jnnBmoGcigMef3GK85zPFcnuDE7x5kC4KnzZqRYGzj1RvU6Tmk3QgxekDCR9aZDVilKFrVood2R/Tr1Zrg1jwQ==;EndpointSuffix=core.windows.net";
+            Task.Run(async () => { await InitializeTable(); })
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        public async Task<List<StudentEntity>> GetAllStudents()
+        {
+            var students = new List<StudentEntity>();
+            TableQuery<StudentEntity> query = new TableQuery<StudentEntity>();
+            TableContinuationToken token = null;
+            do
+            {
+                TableQuerySegment<StudentEntity> resultSegment = await _studentsTable.ExecuteQuerySegmentedAsync(query, token);
+                token = resultSegment.ContinuationToken;
+                students.AddRange(resultSegment);
+            } while (token != null);
+            return students;
+        }
+    }
+}
